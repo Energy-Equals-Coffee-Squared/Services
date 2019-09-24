4e940ded89fd1ecd3bd1e1c379544d6cf03ce465
Andrew Walls <andrewthe9@gmail.com>
2019-09-24 22:59:19 +0200

    New Service
    these are the redone services i will upload the rest tomrow once complete

---
+++ E=MCService/E=MCService/IUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace E_MCService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IUser
+    {
+
+        [OperationContract]
+        int Login(string userName, string passWord);
+        [OperationContract]
+        int Register(UserClass userClass);
+
+        // TODO: Add your service operations here
+    }
+
+
+   
+    
+}
---
+++ E=MCService/E=MCService/ProductService.svc.cs
@@ -0,0 +1,57 @@
+﻿using E_MCService.App_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace E_MCService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ProductService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ProductService.svc or ProductService.svc.cs at the Solution Explorer and start debugging.
+    public class ProductService : IProductService
+    {
+        DataTableDataContext db = new DataTableDataContext();
+
+        public int FindProduct(string name)
+        {
+            var FoundPart = (from p in db.products
+                             where p.name.Equals(name)
+                             select p).FirstOrDefault();
+
+            if (FoundPart != null)
+            {
+                return FoundPart.Id;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public ProductClass GetProduct(string ID)
+        {
+            var product = (from p in db.products
+                             where p.Id.Equals(ID)
+                             select p).FirstOrDefault();
+
+            ProductClass PC = new ProductClass
+            {
+                name = product.name,
+                desc = product.desc,
+                region = product.region,
+                roast = product.roast,
+                altitude_max = product.altitude_max,
+                //not sure why its not picking up altitude min will check tomro
+               // altitude_min = product.altitude_min,
+                created_at = Convert.ToDateTime(product.created_at),
+                updated_at = Convert.ToDateTime(product.updated_at),
+                isDeleted = Convert.ToByte(product.isDeleted),
+                bean_type = product.bean_type
+            };
+
+            return PC;
+        }
+    }
+}
